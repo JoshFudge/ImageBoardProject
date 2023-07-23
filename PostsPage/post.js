9d9bd56e590eed37c0f55a8d68e97d3a1f874b8e
JoshFudge <99412559+JoshFudge@users.noreply.github.com>
2023-07-23 21:28:58 -0230

    added some error handling to searchbars

--- PostsPage/post.js
+++ PostsPage/post.js
@@ -12,6 +12,53 @@ const makeComment = () => {
     
 }
 
+
+const valiateSideBarSearch = (userInput) => {
+    let desiredtags = userInput.split(",")
+    if(!userInput == "" && desiredtags.length != 0 ){
+        return true;
+    }
+}
+
+
+const validateComment = (UserComment) => {
+    if(!UserComment == ""){
+        return true;
+    }
+}
+
+
+
+
 $(document).ready( () => {
+
+    $("#NavbarSearchButton").click(() => {
+        let tagSearch = $("#NavbarSearchBar").val()
+        if(valiateSideBarSearch(tagSearch)){
+            // Do the tag Search
+            $("#navbarSearchError").text("")
+        } else{
+            $("#navbarSearchError").text("Please Enter your Desired tags seperated by a ,")
+        }
+    })
+
+
+
+
+
+
+    $("#postComment").click( () => {
+        let comment = $("#commentArea").val();
+        console.log(comment)
+
+        if(validateComment(comment)){
+            // Do the posting
+        } else{
+            $("#commentError").text("Please Enter a comment before posting!");
+        }
+        
+    } )
+
+
 
 })
