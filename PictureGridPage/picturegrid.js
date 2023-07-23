a51f71c32f4b8612d962f43295defd64d60cccf3
Aaron <aaronnoraator@gmail.com>
2023-07-23 21:26:49 -0230

    Trying this again

--- PictureGridPage/picturegrid.js
+++ PictureGridPage/picturegrid.js
@@ -1,7 +1,11 @@
 "use strict";
-
+//Should getTags take a list of tags from the search bar?
 const getTags = () => {
-
+//Maybe needs to have a for-loop in here somewhere? eg. for each tag in the list, it does the code displayed below?
+    $(`<div class="grid-item">`+
+    `<img id = "photo" src = ${url}>`+
+`</div>`)
+    .appendTo('#grid-container');
 }
 
 const openPost = () => {
@@ -21,3 +25,8 @@ const getImages = async (imageID) => {
     }
     return image
 }
+
+
+
+
+
