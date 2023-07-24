5aaca53c48e9f2bf97206ab3d014776d44abc512
JoshFudge <99412559+JoshFudge@users.noreply.github.com>
2023-07-23 21:41:35 -0230

    adding the Navbar Validation to the other pages

--- Homepage/homepage.js
+++ Homepage/homepage.js
@@ -8,7 +8,41 @@ const getTags = () => {
 
 }
 
+
+
+
+
+const valiateSideBarSearch = (userInput) => {
+    let desiredtags = userInput.split(",")
+    if(!userInput == "" && desiredtags.length != 0 ){
+        return true;
+    }
+}
+
+
+const validateComment = (UserComment) => {
+    if(!UserComment == ""){
+        return true;
+    }
+}
+
 $(document).ready( () => {
+
+    // Getting the tags frm the Navbar Search and doing proper validation
+    $("#NavbarSearchButton").click(() => {
+        let tagSearch = $("#NavbarSearchBar").val()
+        if(valiateSideBarSearch(tagSearch)){
+            // Do the tag Search
+            $("#navbarSearchError").text("")
+        } else{
+            $("#navbarSearchError").text("Please Enter your Desired tags seperated by a ,")
+        }
+    })
+
+
+
+
+
     $("#homepagebutton").click(() => {
         if ($("#homepagebar").val() == ""){
             $("#error").text("Please enter something"); 
--- PictureGridPage/picturegrid.js
+++ PictureGridPage/picturegrid.js
@@ -1,4 +1,12 @@
 "use strict";
+
+const valiateSideBarSearch = (userInput) => {
+    let desiredtags = userInput.split(",")
+    if(!userInput == "" && desiredtags.length != 0 ){
+        return true;
+    }
+}
+
 //Should getTags take a list of tags from the search bar?
 const getTags = () => {
 //Maybe needs to have a for-loop in here somewhere? eg. for each tag in the list, it does the code displayed below?
@@ -26,7 +34,19 @@ const getImages = async (imageID) => {
     return image
 }
 
+$(document).ready( () => {
 
+    $("#NavbarSearchButton").click(() => {
+        let tagSearch = $("#NavbarSearchBar").val()
+        if(valiateSideBarSearch(tagSearch)){
+            // Do the tag Search
+            $("#navbarSearchError").text("")
+        } else{
+            $("#navbarSearchError").text("Please Enter your Desired tags seperated by a ,")
+        }
+    })
+
+})
 
 
 
